9445aa4a1839b7e4ac8e76013d3c641d643aecd1
bartu-g <bartu.girgin12@gmail.com>
2026-01-18 15:52:14 +0300

    add insertion sort to sorting_algorithms.c

--- made_in_c/sorting_algorithms.c
+++ made_in_c/sorting_algorithms.c
@@ -63,3 +63,20 @@ void bubble_sort(int arr[], int n)
         n--;
     } while (swapped);
 }
+
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+    }
+}
