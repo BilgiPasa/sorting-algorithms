c3bbc5d464553732c09678cd69b99464d00eade4
bartu-g <bartu.girgin12@gmail.com>
2026-01-19 19:57:54 +0300

    add sorting_algorithms.h

--- made_in_c/sorting_algorithms.h
+++ made_in_c/sorting_algorithms.h
@@ -1,1 +1,9 @@
-// .h thing here
+#pragma once
+
+void print_array(const int arr[], int n);
+void swap(int *a, int *b);
+int index_of_min(const int arr[], int n, int start);
+void bubble_sort(int arr[], int n);
+void gnome_sort(int arr[], int n);
+void insertion_sort(int arr[], int n);
+void selection_sort(int arr[], int n);
