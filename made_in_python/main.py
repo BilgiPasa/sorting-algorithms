59959c05d02d58e02c3943fbdb60a0957782ec95
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2026-02-17 01:35:49 +0300

    Removing classes in Python because it does not need it. Just using functions is enough i think.

--- made_in_python/main.py
+++ made_in_python/main.py
@@ -1,8 +1,8 @@
+from array import array
 from enum import IntEnum
 from random import randint
+from sorting_algorithms import *
 from time import time
-import array as a
-import sorting_algorithms as s
 
 class AlgorithmTypes(IntEnum):
     BUILT_IN = 0
@@ -18,203 +18,203 @@ class AlgorithmTypes(IntEnum):
     BOZO_SORT = 10
     BOGO_SORT = 11
 
-class Main:
-    def main() -> None:
-        algorithm_type: AlgorithmTypes
-        print()
-        print("Sorting algorithms (from slowest to fastest)")
-        print("1) Quick Sort")
-        print("2) Merge Sort")
-        print("3) Shell Sort")
-        print("4) Insertsion Sort")
-        print("5) Selection Sort")
-        print("6) Gnome Sort")
-        print("7) Cocktail Shaker Sort")
-        print("8) Bubble Sort")
-        print("9) Sootage Sort")
-        print("10) Bozo Sort")
-        print("11) Bogo Sort")
 
-        try:
-            selected_algorithm: int = int(input("Select an algorithm: "))
-        except:
-            print("Couldn't understand the input. Aborting.")
+def main() -> None:
+    algorithm_type: AlgorithmTypes
+    print()
+    print("Sorting algorithms (from slowest to fastest)")
+    print("1) Quick Sort")
+    print("2) Merge Sort")
+    print("3) Shell Sort")
+    print("4) Insertsion Sort")
+    print("5) Selection Sort")
+    print("6) Gnome Sort")
+    print("7) Cocktail Shaker Sort")
+    print("8) Bubble Sort")
+    print("9) Sootage Sort")
+    print("10) Bozo Sort")
+    print("11) Bogo Sort")
+
+    try:
+        selected_algorithm: int = int(input("Select an algorithm: "))
+    except:
+        print("Couldn't understand the input. Aborting.")
+        return
+
+    if selected_algorithm == AlgorithmTypes.BUILT_IN:
+        algorithm_type = AlgorithmTypes.BUILT_IN
+    elif selected_algorithm == AlgorithmTypes.QUICK_SORT:
+        algorithm_type = AlgorithmTypes.QUICK_SORT
+    elif selected_algorithm == AlgorithmTypes.MERGE_SORT:
+        algorithm_type = AlgorithmTypes.MERGE_SORT
+    elif selected_algorithm == AlgorithmTypes.SHELL_SORT:
+        algorithm_type = AlgorithmTypes.SHELL_SORT
+    elif selected_algorithm == AlgorithmTypes.INSERTSION_SORT:
+        algorithm_type = AlgorithmTypes.INSERTSION_SORT
+    elif selected_algorithm == AlgorithmTypes.SELECTION_SORT:
+        algorithm_type = AlgorithmTypes.SELECTION_SORT
+    elif selected_algorithm == AlgorithmTypes.GNOME_SORT:
+        algorithm_type = AlgorithmTypes.GNOME_SORT
+    elif selected_algorithm == AlgorithmTypes.COCKTAIL_SHAKER_SORT:
+        algorithm_type = AlgorithmTypes.COCKTAIL_SHAKER_SORT
+    elif selected_algorithm == AlgorithmTypes.BUBBLE_SORT:
+        algorithm_type = AlgorithmTypes.BUBBLE_SORT
+    elif selected_algorithm == AlgorithmTypes.SOOTAGE_SORT:
+        algorithm_type = AlgorithmTypes.SOOTAGE_SORT
+    elif selected_algorithm == AlgorithmTypes.BOZO_SORT:
+        algorithm_type = AlgorithmTypes.BOZO_SORT
+    elif selected_algorithm == AlgorithmTypes.BOGO_SORT:
+        algorithm_type = AlgorithmTypes.BOGO_SORT
+    else:
+        print("Couldn't understand the input. Aborting.")
+        return
+
+    match algorithm_type:
+        case AlgorithmTypes.BUILT_IN:
+            print("Enter the array size (12345678 is recommended): ", end="")
+
+        case AlgorithmTypes.QUICK_SORT | AlgorithmTypes.MERGE_SORT | AlgorithmTypes.SHELL_SORT:
+            print("Enter the array size (less than 12345678 is recommended): ", end="")
+
+        case AlgorithmTypes.INSERTSION_SORT | AlgorithmTypes.SELECTION_SORT | AlgorithmTypes.GNOME_SORT | AlgorithmTypes.COCKTAIL_SHAKER_SORT | AlgorithmTypes.BUBBLE_SORT:
+            print("Enter the array size (less than 55555 is recommended): ", end="")
+
+        case AlgorithmTypes.SOOTAGE_SORT:
+            print("Enter the array size (less than 3223 is recommended): ", end="")
+
+        case AlgorithmTypes.BOZO_SORT | AlgorithmTypes.BOGO_SORT:
+            print("Enter the array size (less than 12 is recommended): ", end="")
+
+        case _:
+            print("The algorithm type could not found. Aborting.")
             return
 
-        if selected_algorithm == AlgorithmTypes.BUILT_IN:
-            algorithm_type = AlgorithmTypes.BUILT_IN
-        elif selected_algorithm == AlgorithmTypes.QUICK_SORT:
-            algorithm_type = AlgorithmTypes.QUICK_SORT
-        elif selected_algorithm == AlgorithmTypes.MERGE_SORT:
-            algorithm_type = AlgorithmTypes.MERGE_SORT
-        elif selected_algorithm == AlgorithmTypes.SHELL_SORT:
-            algorithm_type = AlgorithmTypes.SHELL_SORT
-        elif selected_algorithm == AlgorithmTypes.INSERTSION_SORT:
-            algorithm_type = AlgorithmTypes.INSERTSION_SORT
-        elif selected_algorithm == AlgorithmTypes.SELECTION_SORT:
-            algorithm_type = AlgorithmTypes.SELECTION_SORT
-        elif selected_algorithm == AlgorithmTypes.GNOME_SORT:
-            algorithm_type = AlgorithmTypes.GNOME_SORT
-        elif selected_algorithm == AlgorithmTypes.COCKTAIL_SHAKER_SORT:
-            algorithm_type = AlgorithmTypes.COCKTAIL_SHAKER_SORT
-        elif selected_algorithm == AlgorithmTypes.BUBBLE_SORT:
-            algorithm_type = AlgorithmTypes.BUBBLE_SORT
-        elif selected_algorithm == AlgorithmTypes.SOOTAGE_SORT:
-            algorithm_type = AlgorithmTypes.SOOTAGE_SORT
-        elif selected_algorithm == AlgorithmTypes.BOZO_SORT:
-            algorithm_type = AlgorithmTypes.BOZO_SORT
-        elif selected_algorithm == AlgorithmTypes.BOGO_SORT:
-            algorithm_type = AlgorithmTypes.BOGO_SORT
-        else:
-            print("Couldn't understand the input. Aborting.")
+    # Why do I write "less than" at the part above? Because Python is slow. I am not joking.
+    # But fortunately, Python's built-in sorter is fast enough.
+
+    length: int
+
+    try:
+        length = int(input())
+    except:
+        print("Wrong input. Aborting.")
+        return
+
+    MAX_VALUE: int = 2147483647 # 32-bit signed integer max value
+
+    if length == 0:
+        print("Don't make the array size as 0. Aborting.")
+        return
+    elif length > MAX_VALUE:
+        print("Don't make the array size more than MAX_VALUE. Aborting.")
+        return
+
+    try:
+        num_arr = array('i', [0] * length)
+    except:
+        print("The array size cannot be " + str(length) + ". Aborting.")
+        return
+
+    print("Starting to randomize the array.")
+
+    # Python's integer is 64 bit. Because of that I made the randomizer like this.
+    for i in range(len(num_arr)):
+        # Note: the randint function includes both end points.
+        num_arr[i] = randint(0, MAX_VALUE)
+        # Don't forget to write MAX_VALUE as the endpoint.
+
+    print("The array has randomized.")
+    #print("[" + ", ".join(str(x) for x in num_arr) + "]") # To see the array before sorting
+    start_time: float
+    end_time: float
+    used_algorithm_type: str
+    print("Starting to sort the array.")
+
+    match algorithm_type:
+        case AlgorithmTypes.BUILT_IN:
+            start_time = time()
+            num_arr = sorted(num_arr)
+            end_time = time()
+            used_algorithm_type = "Python's built-in sorter"
+
+        case AlgorithmTypes.QUICK_SORT:
+            start_time = time()
+            quick_sort(num_arr, 0, len(num_arr) - 1)
+            end_time = time()
+            used_algorithm_type = "Quick Sort"
+
+        case AlgorithmTypes.MERGE_SORT:
+            start_time = time()
+            merge_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Merge Sort"
+
+        case AlgorithmTypes.SHELL_SORT:
+            start_time = time()
+            shell_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Shell Sort"
+
+        case AlgorithmTypes.INSERTSION_SORT:
+            start_time = time()
+            insertsion_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Insertsion Sort"
+
+        case AlgorithmTypes.SELECTION_SORT:
+            start_time = time()
+            selection_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Selection Sort"
+
+        case AlgorithmTypes.GNOME_SORT:
+            start_time = time()
+            gnome_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Gnome Sort"
+
+        case AlgorithmTypes.COCKTAIL_SHAKER_SORT:
+            start_time = time()
+            cocktail_shaker_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Cocktail Shaker Sort"
+
+        case AlgorithmTypes.BUBBLE_SORT:
+            start_time = time()
+            bubble_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Bubble Sort"
+
+        case AlgorithmTypes.SOOTAGE_SORT:
+            start_time = time()
+            sootage_sort(num_arr, 0, len(num_arr) - 1)
+            end_time = time()
+            used_algorithm_type = "Sootage Sort"
+
+        case AlgorithmTypes.BOZO_SORT:
+            start_time = time()
+            bozo_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Bozo Sort"
+
+        case AlgorithmTypes.BOGO_SORT:
+            start_time = time()
+            bogo_sort(num_arr)
+            end_time = time()
+            used_algorithm_type = "Bogo Sort"
+
+        case _:
+            print("The algorithm type could not found. Aborting.")
             return
 
-        match algorithm_type:
-            case AlgorithmTypes.BUILT_IN:
-                print("Enter the array size (12345678 is recommended): ", end="")
+    #print("[" + ", ".join(str(x) for x in num_arr) + "]") # To see the array after sorting
 
-            case AlgorithmTypes.QUICK_SORT | AlgorithmTypes.MERGE_SORT | AlgorithmTypes.SHELL_SORT:
-                print("Enter the array size (less than 12345678 is recommended): ", end="")
+    if not is_sorted(num_arr):
+        print("The sorting algorithm ran but the array is not fully sorted.")
+        return
 
-            case AlgorithmTypes.INSERTSION_SORT | AlgorithmTypes.SELECTION_SORT | AlgorithmTypes.GNOME_SORT | AlgorithmTypes.COCKTAIL_SHAKER_SORT | AlgorithmTypes.BUBBLE_SORT:
-                print("Enter the array size (less than 55555 is recommended): ", end="")
+    print(str(len(num_arr)) + " random integers has been sorted in " + str((end_time - start_time) * 1000) + " milliseconds using " + used_algorithm_type + ".")
+    print()
 
-            case AlgorithmTypes.SOOTAGE_SORT:
-                print("Enter the array size (less than 3223 is recommended): ", end="")
-
-            case AlgorithmTypes.BOZO_SORT | AlgorithmTypes.BOGO_SORT:
-                print("Enter the array size (less than 12 is recommended): ", end="")
-
-            case _:
-                print("The algorithm type could not found. Aborting.")
-                return
-
-        # Why do I write "less than" at the part above? Because Python is slow. I am not joking.
-        # But fortunately, Python's built-in sorter is fast enough.
-
-        length: int
-
-        try:
-            length = int(input())
-        except:
-            print("Wrong input. Aborting.")
-            return
-
-        MAX_VALUE: int = 2147483647 # 32-bit signed integer max value
-
-        if length == 0:
-            print("Don't make the array size as 0. Aborting.")
-            return
-        elif length > MAX_VALUE:
-            print("Don't make the array size more than MAX_VALUE. Aborting.")
-            return
-
-        try:
-            num_arr = a.array('i', [0] * length)
-        except:
-            print("The array size cannot be " + str(length) + ". Aborting.")
-            return
-
-        print("Starting to randomize the array.")
-
-        # Python's integer is 64 bit. Because of that I made the randomizer like this.
-        for i in range(len(num_arr)):
-            # Note: the randint function includes both end points.
-            num_arr[i] = randint(0, MAX_VALUE)
-            # Don't forget to write MAX_VALUE as the endpoint.
-
-        print("The array has randomized.")
-        #print("[" + ", ".join(str(x) for x in num_arr) + "]") # To see the array before sorting
-        start_time: float
-        end_time: float
-        used_algorithm_type: str
-        print("Starting to sort the array.")
-
-        match algorithm_type:
-            case AlgorithmTypes.BUILT_IN:
-                start_time = time()
-                num_arr = sorted(num_arr)
-                end_time = time()
-                used_algorithm_type = "Python's built-in sorter"
-
-            case AlgorithmTypes.QUICK_SORT:
-                start_time = time()
-                s.SortingAlgorithms.quick_sort(num_arr, 0, len(num_arr) - 1)
-                end_time = time()
-                used_algorithm_type = "Quick Sort"
-
-            case AlgorithmTypes.MERGE_SORT:
-                start_time = time()
-                s.SortingAlgorithms.merge_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Merge Sort"
-
-            case AlgorithmTypes.SHELL_SORT:
-                start_time = time()
-                s.SortingAlgorithms.shell_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Shell Sort"
-
-            case AlgorithmTypes.INSERTSION_SORT:
-                start_time = time()
-                s.SortingAlgorithms.insertsion_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Insertsion Sort"
-
-            case AlgorithmTypes.SELECTION_SORT:
-                start_time = time()
-                s.SortingAlgorithms.selection_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Selection Sort"
-
-            case AlgorithmTypes.GNOME_SORT:
-                start_time = time()
-                s.SortingAlgorithms.gnome_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Gnome Sort"
-
-            case AlgorithmTypes.COCKTAIL_SHAKER_SORT:
-                start_time = time()
-                s.SortingAlgorithms.cocktail_shaker_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Cocktail Shaker Sort"
-
-            case AlgorithmTypes.BUBBLE_SORT:
-                start_time = time()
-                s.SortingAlgorithms.bubble_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Bubble Sort"
-
-            case AlgorithmTypes.SOOTAGE_SORT:
-                start_time = time()
-                s.SortingAlgorithms.sootage_sort(num_arr, 0, len(num_arr) - 1)
-                end_time = time()
-                used_algorithm_type = "Sootage Sort"
-
-            case AlgorithmTypes.BOZO_SORT:
-                start_time = time()
-                s.SortingAlgorithms.bozo_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Bozo Sort"
-
-            case AlgorithmTypes.BOGO_SORT:
-                start_time = time()
-                s.SortingAlgorithms.bogo_sort(num_arr)
-                end_time = time()
-                used_algorithm_type = "Bogo Sort"
-
-            case _:
-                print("The algorithm type could not found. Aborting.")
-                return
-
-        #print("[" + ", ".join(str(x) for x in num_arr) + "]") # To see the array after sorting
-
-        if not s.SortingAlgorithms.is_sorted(num_arr):
-            print("The sorting algorithm ran but the array is not fully sorted.")
-            return
-
-        print(str(len(num_arr)) + " random integers has been sorted in " + str((end_time - start_time) * 1000) + " milliseconds using " + used_algorithm_type + ".")
-        print()
-
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
--- made_in_python/sorting_algorithms.py
+++ made_in_python/sorting_algorithms.py
@@ -1,194 +1,193 @@
+from main import main
 from random import randint, shuffle
-import main
 
-class SortingAlgorithms:
-    def is_sorted(num_arr) -> bool: # To check if the array is sorted
-        for i in range(1, len(num_arr)):
-            if num_arr[i - 1] > num_arr[i]:
-                return False
+def is_sorted(num_arr) -> bool: # To check if the array is sorted
+    for i in range(1, len(num_arr)):
+        if num_arr[i - 1] > num_arr[i]:
+            return False
 
-        return True
+    return True
 
-    # The algorithms are listed from the fastest to the slowest according to my tests.
+# The algorithms are listed from the fastest to the slowest according to my tests.
 
-    def quick_sort(num_arr, start: int, end: int) -> None:
-        if start >= end:
-            return
+def quick_sort(num_arr, start: int, end: int) -> None:
+    if start >= end:
+        return
 
-        pivot: int = SortingAlgorithms.move_elements_and_return_pivot(num_arr, start, end)
-        SortingAlgorithms.quick_sort(num_arr, start, pivot - 1)
-        SortingAlgorithms.quick_sort(num_arr, pivot + 1, end)
+    pivot: int = move_elements_and_return_pivot(num_arr, start, end)
+    quick_sort(num_arr, start, pivot - 1)
+    quick_sort(num_arr, pivot + 1, end)
 
-    def move_elements_and_return_pivot(num_arr, start: int, end: int) -> int: # This is for Quick Sort
-        i: int = start - 1
+def move_elements_and_return_pivot(num_arr, start: int, end: int) -> int: # This is for Quick Sort
+    i: int = start - 1
 
-        for j in range(start, end): # Initial pivot is the last element of the array
-            # The program moves the elements that are smaller than the pivot to the left
-            if num_arr[j] < num_arr[end]:
-                i += 1
-                num_arr[i], num_arr[j] = num_arr[j], num_arr[i] # Swapping elements
+    for j in range(start, end): # Initial pivot is the last element of the array
+        # The program moves the elements that are smaller than the pivot to the left
+        if num_arr[j] < num_arr[end]:
+            i += 1
+            num_arr[i], num_arr[j] = num_arr[j], num_arr[i] # Swapping elements
 
-        i += 1
-        num_arr[i], num_arr[end] = num_arr[end], num_arr[i] # Swapping elements
-        return i # Swapped the initial pivot with the new pivot and returning the new pivot
+    i += 1
+    num_arr[i], num_arr[end] = num_arr[end], num_arr[i] # Swapping elements
+    return i # Swapped the initial pivot with the new pivot and returning the new pivot
 
-    def merge_sort(num_arr) -> None:
-        if len(num_arr) < 2:
-            return
+def merge_sort(num_arr) -> None:
+    if len(num_arr) < 2:
+        return
 
-        middle: int = len(num_arr) // 2
-        i: int
-        left_arr = [0] * middle
-        right_arr = [0] * (len(num_arr) - middle)
+    middle: int = len(num_arr) // 2
+    i: int
+    left_arr = [0] * middle
+    right_arr = [0] * (len(num_arr) - middle)
 
-        for i in range(middle):
-            left_arr[i] = num_arr[i]
+    for i in range(middle):
+        left_arr[i] = num_arr[i]
 
-        for i in range(middle, len(num_arr)):
-            right_arr[i - middle] = num_arr[i]
+    for i in range(middle, len(num_arr)):
+        right_arr[i - middle] = num_arr[i]
 
-        SortingAlgorithms.merge_sort(left_arr)
-        SortingAlgorithms.merge_sort(right_arr)
-        SortingAlgorithms.merge(num_arr, left_arr, right_arr)
+    merge_sort(left_arr)
+    merge_sort(right_arr)
+    merge(num_arr, left_arr, right_arr)
 
-    def merge(num_arr, left_arr, right_arr) -> None: # This is for Merge Sort
-        j: int = 0
-        k: int = 0
+def merge(num_arr, left_arr, right_arr) -> None: # This is for Merge Sort
+    j: int = 0
+    k: int = 0
 
-        for i in range(len(num_arr)):
-            if j >= len(left_arr):
-                num_arr[i] = right_arr[k]
-                k += 1
-            elif k >= len(right_arr):
+    for i in range(len(num_arr)):
+        if j >= len(left_arr):
+            num_arr[i] = right_arr[k]
+            k += 1
+        elif k >= len(right_arr):
+            num_arr[i] = left_arr[j]
+            j += 1
+        else:
+            if left_arr[j] < right_arr[k]:
                 num_arr[i] = left_arr[j]
                 j += 1
             else:
-                if left_arr[j] < right_arr[k]:
-                    num_arr[i] = left_arr[j]
-                    j += 1
-                else:
-                    num_arr[i] = right_arr[k]
-                    k += 1
+                num_arr[i] = right_arr[k]
+                k += 1
 
-    def shell_sort(num_arr) -> None:
-        interval: int = len(num_arr) // 2
-        temp: int
-        j: int
+def shell_sort(num_arr) -> None:
+    interval: int = len(num_arr) // 2
+    temp: int
+    j: int
 
-        while interval > 0:
-            for i in range(interval, len(num_arr)):
-                temp = num_arr[i]
-                j = i
-
-                while j >= interval and num_arr[j - interval] > temp:
-                    num_arr[j] = num_arr[j - interval]
-                    j -= interval
-
-                num_arr[j] = temp
-            interval = interval // 2
-
-    def insertsion_sort(num_arr) -> None:
-        temp: int
-        j: int
-
-        for i in range(1, len(num_arr)):
+    while interval > 0:
+        for i in range(interval, len(num_arr)):
             temp = num_arr[i]
-            j = i - 1
+            j = i
 
-            while j >= 0 and num_arr[j] > temp:
-                num_arr[j + 1] = num_arr[j]
-                j -= 1
+            while j >= interval and num_arr[j - interval] > temp:
+                num_arr[j] = num_arr[j - interval]
+                j -= interval
 
-            num_arr[j + 1] = temp
+            num_arr[j] = temp
+        interval = interval // 2
 
-    def selection_sort(num_arr) -> None:
-        length_minus_one: int = len(num_arr) - 1
-        j: int
+def insertsion_sort(num_arr) -> None:
+    temp: int
+    j: int
 
-        for i in range(length_minus_one):
-            j = SortingAlgorithms.index_of_min(num_arr, len(num_arr), i)
-            num_arr[i], num_arr[j] = num_arr[j], num_arr[i] # Swapping elements
+    for i in range(1, len(num_arr)):
+        temp = num_arr[i]
+        j = i - 1
 
-    def index_of_min(num_arr, length: int, start: int) -> None: # This is for Selection Sort
-        min: int = start
+        while j >= 0 and num_arr[j] > temp:
+            num_arr[j + 1] = num_arr[j]
+            j -= 1
 
-        for i in range(start, length):
-            if num_arr[min] > num_arr[i]:
-                min = i
+        num_arr[j + 1] = temp
 
-        return min
+def selection_sort(num_arr) -> None:
+    length_minus_one: int = len(num_arr) - 1
+    j: int
 
-    def gnome_sort(num_arr) -> None:
-        i: int = 1
+    for i in range(length_minus_one):
+        j = index_of_min(num_arr, len(num_arr), i)
+        num_arr[i], num_arr[j] = num_arr[j], num_arr[i] # Swapping elements
 
-        while i < len(num_arr):
-            if i > 0 and num_arr[i - 1] > num_arr[i]:
+def index_of_min(num_arr, length: int, start: int) -> None: # This is for Selection Sort
+    min: int = start
+
+    for i in range(start, length):
+        if num_arr[min] > num_arr[i]:
+            min = i
+
+    return min
+
+def gnome_sort(num_arr) -> None:
+    i: int = 1
+
+    while i < len(num_arr):
+        if i > 0 and num_arr[i - 1] > num_arr[i]:
+            num_arr[i - 1], num_arr[i] = num_arr[i], num_arr[i - 1] # Swapping elements
+            i -= 1
+        else:
+            i += 1
+
+def cocktail_shaker_sort(num_arr) -> None:
+    start: int = 0
+    end: int = len(num_arr) - 1
+    i: int
+
+    while end - start > 1:
+        for i in range(start, end):
+            if num_arr[i] > num_arr[i + 1]:
+                num_arr[i], num_arr[i + 1] = num_arr[i + 1], num_arr[i] # Swapping elements
+
+        end -= 1
+
+        for i in range(end, start, -1):
+            if num_arr[i - 1] > num_arr[i]:
                 num_arr[i - 1], num_arr[i] = num_arr[i], num_arr[i - 1] # Swapping elements
-                i -= 1
-            else:
-                i += 1
 
-    def cocktail_shaker_sort(num_arr) -> None:
-        start: int = 0
-        end: int = len(num_arr) - 1
-        i: int
+        start += 1
 
-        while end - start > 1:
-            for i in range(start, end):
-                if num_arr[i] > num_arr[i + 1]:
-                    num_arr[i], num_arr[i + 1] = num_arr[i + 1], num_arr[i] # Swapping elements
+def bubble_sort(num_arr) -> None:
+    length: int = len(num_arr)
+    swapped: bool
 
-            end -= 1
+    while True:
+        swapped = False
 
-            for i in range(end, start, -1):
-                if num_arr[i - 1] > num_arr[i]:
-                    num_arr[i - 1], num_arr[i] = num_arr[i], num_arr[i - 1] # Swapping elements
+        for i in range(1, length):
+            if num_arr[i - 1] > num_arr[i]:
+                num_arr[i - 1], num_arr[i] = num_arr[i], num_arr[i - 1] # Swapping elements
+                swapped = True
 
-            start += 1
+        length -= 1
 
-    def bubble_sort(num_arr) -> None:
-        length: int = len(num_arr)
-        swapped: bool
+        if not swapped:
+            break
 
-        while True:
-            swapped = False
+def sootage_sort(num_arr, start: int, end: int) -> None:
+    if start == end:
+        return
 
-            for i in range(1, len(length)):
-                if num_arr[i - 1] > num_arr[i]:
-                    num_arr[i - 1], num_arr[i] = num_arr[i], num_arr[i - 1] # Swapping elements
-                    swapped = True
+    if num_arr[start] > num_arr[end]:
+        num_arr[start], num_arr[end] = num_arr[end], num_arr[start] # Swapping elements
 
-            length -= 1
+    if end - start > 1: # This means if array size > 2
+        one_third: int = (end - start + 1) // 3
+        sootage_sort(num_arr, start, end - one_third)
+        sootage_sort(num_arr, start + one_third, end)
+        sootage_sort(num_arr, start, end - one_third)
 
-            if not swapped:
-                break
+def bozo_sort(num_arr) -> None:
+    length_minus_one: int = len(num_arr) - 1
+    index1: int
+    index2: int
 
-    def sootage_sort(num_arr, start: int, end: int) -> None:
-        if start == end:
-            return
+    while not is_sorted(num_arr):
+        index1 = randint(0, length_minus_one)
+        index2 = randint(0, length_minus_one)
+        num_arr[index1], num_arr[index2] = num_arr[index2], num_arr[index1] # Swapping elements
 
-        if num_arr[start] > num_arr[end]:
-            num_arr[start], num_arr[end] = num_arr[end], num_arr[start] # Swapping elements
+def bogo_sort(num_arr) -> None:
+    while not is_sorted(num_arr):
+        shuffle(num_arr)
 
-        if end - start > 1: # This means if array size > 2
-            one_third: int = (end - start + 1) // 3
-            SortingAlgorithms.sootage_sort(num_arr, start, end - one_third)
-            SortingAlgorithms.sootage_sort(num_arr, start + one_third, end)
-            SortingAlgorithms.sootage_sort(num_arr, start, end - one_third)
-
-    def bozo_sort(num_arr) -> None:
-        length_minus_one: int = len(num_arr) - 1
-        index1: int
-        index2: int
-
-        while not SortingAlgorithms.is_sorted(num_arr):
-            index1 = randint(0, length_minus_one)
-            index2 = randint(0, length_minus_one)
-            num_arr[index1], num_arr[index2] = num_arr[index2], num_arr[index1] # Swapping elements
-
-    def bogo_sort(num_arr) -> None:
-        while not SortingAlgorithms.is_sorted(num_arr):
-            shuffle(num_arr)
-
-    if __name__ == "__main__":
-        main.Main.main()
+if __name__ == "__main__":
+    main()
