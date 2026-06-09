b9a019cdd16b9d994bce36086993de710eab0c52
bartu <bartu.girgin12@gmail.com>
2026-06-09 15:18:37 +0300

    fix spacing in main.c

--- made_in_c/sorting_algorithms.h
+++ made_in_c/sorting_algorithms.h
@@ -1,8 +1,7 @@
 #pragma once
-#include <stdbool.h>
 
 void print_arr(const int num_arr[], int length);
-bool is_sorted(const int num_arr[], int length);
+int is_sorted(const int num_arr[], int length);
 void swap(int *a, int *b);
 int compare(const void *elem1, const void *elem2);
 int merge_sort(int num_arr[], int length);
